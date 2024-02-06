cbb74857227aa317dfe73c6a51b4ff20f5b008f8
Junho <soarnsoar@gmail.com>
2024-02-06 17:18:52 +0900

    add __init__.py

--- config/TTsemilep_ChargeReliability/input.py
+++ config/TTsemilep_ChargeReliability/input.py
@@ -9,4 +9,11 @@ dict_input={
     "ElectronLepAcc":maindir2+"RunSyst__RunSoftElectron__RunLeptonSide__RunChAcc__/combine.root",
     "JetHadAcc":maindir2+"RunSyst__RunJet__RunHadronSide__RunChAcc__/combine.root",
     "JetLepAcc":maindir2+"RunSyst__RunJet__RunLeptonSide__RunChAcc__/combine.root",
+
+    "MuonHadReliab":maindir2+"RunSyst__RunSoftMuon__RunHadronSide__RunRelib__/combine.root",
+    "MuonLepReliab":maindir2+"RunSyst__RunSoftMuon__RunLeptonSide__RunRelib__/combine.root",
+    "ElectronHadReliab":maindir2+"RunSyst__RunSoftElectron__RunHadronSide__RunRelib__/combine.root",
+    "ElectronLepReliab":maindir2+"RunSyst__RunSoftElectron__RunLeptonSide__RunRelib__/combine.root",
+    "JetHadReliab":maindir2+"RunSyst__RunJet__RunHadronSide__RunRelib__/combine.root",
+    "JetLepReliab":maindir2+"RunSyst__RunJet__RunLeptonSide__RunRelib__/combine.root",
 }
